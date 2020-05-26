ecf67f18beb56f491283e22e84d9f844bec7d1f6
sergey khvalko <falko.serega2016@yandex.ru>
2020-05-26 23:07:00 +0300

    second commit

--- sctiptReact.js
+++ sctiptReact.js
@@ -1,4 +1,4 @@
-const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -8,14 +8,29 @@ const personalMovieDB = {
     private: false
 };
 
-const a = prompt('один из последних просмотренных фильмов', ''),
-      b = prompt('на сколько оцените его', ''),
-      c = prompt('один из последних просмотренных фильмов', ''),
-      d = prompt('на сколько оцените его', '');
 
+for (let i = 0; i < 2; i++) {
+    const a = prompt('один из последних просмотренных фильмов', ''),
+          b = prompt('на сколько оцените его', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+    if (a !== null && b !== null && a !== '' && b !== '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
 
+if (personalMovieDB.count < 10) {
+    console.log('Просмотренно довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+    console.log('Вы киноман');
+} else {
+    console.log('Произошла ошибка');
+}
 
 console.log(personalMovieDB);
+
